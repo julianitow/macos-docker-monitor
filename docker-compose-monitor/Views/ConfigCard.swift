734a49accbd295734975b8a2f1d7c2b6c6daf34d
Julien Guillan <jguillan@medissimo.fr>
2023-06-24 16:08:04 +0200

    Config card with edit button + add config

--- docker-compose-monitor/Views/ConfigCard.swift
+++ docker-compose-monitor/Views/ConfigCard.swift
@@ -11,28 +11,38 @@ struct ConfigCard: View {
     @Binding var config: Config
     
     var body: some View {
-        VStack {
-            Text(config.name.uppercased())
-                .font(.title)
-                .fontWeight(.bold)
-                .frame(width: 200)
-                .foregroundColor(.black)
-            
-            Spacer()
-            
-            Image(systemName: "pencil")
-                .font(.title)
-                .foregroundColor(.black)
-                .padding()
-            
-            Text(config.host)
-                .font(.subheadline)
-                .foregroundColor(.black)
-                .padding(.bottom)
+        VStack (alignment: .leading) {
+            HStack {
+                Text(config.name.uppercased())
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .frame(width: 100)
+                    .foregroundColor(.black)
+                Spacer()
+                Image(systemName: "pencil")
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .padding()
+                    .onTapGesture {
+                        let alert = NSAlert()
+                        alert.informativeText = "Coming soon"
+                        alert.addButton(withTitle: "Roger that.")
+                        alert.alertStyle = .warning
+                        alert.runModal()
+                    }
+            }
+            .frame(width: 200)
+            HStack {
+                Text(config.host)
+                    .font(.subheadline)
+                    .foregroundColor(.black)
+                    .padding(.horizontal)
+                    .padding(.bottom)
+            }
         }
         .background(config.isSelected ? .blue : config.isConnected ? Color(hex: "#2ecc71") : Color(hex: "#95a5a6"))
         .cornerRadius(10)
-        .frame(minWidth: 100, maxWidth: .infinity)
+        .frame(minWidth: 100, maxWidth: 100)
         .fixedSize(horizontal: false, vertical: true)
         .padding()
     }
--- docker-compose-monitor/Views/ContentView.swift
+++ docker-compose-monitor/Views/ContentView.swift
@@ -16,16 +16,27 @@ struct ContentView: View {
             
     var body: some View {
         GeometryReader { geometry in
-            HStack(spacing: 0) {
+            HStack(alignment: .top, spacing: 0) {
                 VStack { // config list
-                    ForEach(Array(configs.enumerated()), id: \.element.id) { (index, config) in
-                        ConfigCard(config: $configs[index])
-                            .gesture(TapGesture(count: 2).onEnded { gesture in
-                                toggleConnection(for: config)
-                            })
-                            .gesture(TapGesture(count: 1).onEnded {
-                                toggleSelection(for: config)
-                            })
+                    Button(action: addConfig) {
+                            Label("Add config", systemImage: "folder.badge.plus")
+                    }
+                    .padding()
+                    Divider()
+                    ScrollView(.vertical) {
+                        VStack (spacing: 5) {
+                            ForEach(Array(configs.enumerated()), id: \.element.id) { (index, config) in
+                                ConfigCard(config: $configs[index])
+                                    .gesture(TapGesture(count: 2).onEnded { gesture in
+                                        toggleConnection(for: config)
+                                    })
+                                    .gesture(TapGesture(count: 1).onEnded {
+                                        toggleSelection(for: config)
+                                    })
+                            }
+                        }
+                        .frame(width: geometry.size.width)
+                        .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                     }
                 }
                 .frame(width: geometry.size.width * firstColumnWidth)
@@ -65,6 +76,11 @@ struct ContentView: View {
                 .padding()
             }
         }
+    }
+    
+    private func addConfig() -> Void {
+        // TODO display a window to add a ne config
+        print("WIP")
     }
     
     private func toggleSelection(for config: Config) -> Void {
