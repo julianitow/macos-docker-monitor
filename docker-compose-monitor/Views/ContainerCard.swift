8cf4838175797334a80e8b4fdc0302f0584d4548
Julien Guillan <jguillan@medissimo.fr>
2023-06-24 16:19:07 +0200

    Add button + action

--- docker-compose-monitor/Views/ContainerCard.swift
+++ docker-compose-monitor/Views/ContainerCard.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContainerCard: View {
     
     @State var container: Container
+    @State var displayLogs: Bool = false
     
     var body: some View {
         VStack {
@@ -20,17 +21,55 @@ struct ContainerCard: View {
                 .foregroundColor(.white)
             
             Spacer()
+            
+            HStack {
+                Button(action: runAction) {
+                    Label("Run", systemImage: "play.fill")
+                }
+                Button(action: stopAction) {
+                    Label("Stop", systemImage: "stop.fill")
+                }
+                Button(action: pullAction) {
+                    Label("Pull", systemImage: "square.and.arrow.down.fill")
+                }
+                Button(action: showLogs) {
+                    Label("Show logs", systemImage: "play.display")
+                }
+            }
                     
             Text(container.status.rawValue)
                 .font(.subheadline)
                 .foregroundColor(.black)
                 .padding(.bottom)
+            
+            if displayLogs {
+                VStack {
+                    Text("Logs")
+                }
+                .frame(width: 600, height: 600)
+            }
         }
         .background(container.status == .RUNNING ? Color(hex: "#2ecc71") : container.status == .EXITED ? Color(hex: "#c0392b") : container.status == .STOPPED ? Color(hex: "#95a5a6") : .white)
         .cornerRadius(10)
-        .frame(minWidth: 100, maxWidth: .infinity)
+        .frame(width: 500)
         .fixedSize(horizontal: false, vertical: true)
         .padding()
+    }
+    
+    private func runAction() -> Void {
+        
+    }
+    
+    private func stopAction() -> Void {
+        
+    }
+    
+    private func pullAction() -> Void {
+        
+    }
+    
+    private func showLogs() -> Void {
+        self.displayLogs.toggle()
     }
 }
 
