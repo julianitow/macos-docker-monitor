47ebd9c4ddc2d22fae21047f1cacce5952bb4eac
Julien Guillan <jguillan@medissimo.fr>
2023-06-24 10:52:02 +0200

    Initial Commit

---
+++ docker-compose-monitor/docker_compose_monitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  docker_compose_monitorApp.swift
+//  docker-compose-monitor
+//
+//  Created by Julien Guillan on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct docker_compose_monitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
